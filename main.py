aefc3e92df5c36e41455fcfa7557075a9e0f9d1e
cc-nina <134860155+cc-nina@users.noreply.github.com>
2024-12-21 21:59:23 +0000

    file uploader args

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
- 
-st.write("""
-# PDF Combiner""")
+
 st.write("""
 # PDF Combiner""")
 
-st.file_uploader(label="pdf", type=['pdf'], accept_multiple_files=True, key=None, disabled=False, label_visibility="visible")
+st.write("""
+Combine your pdfs.""")
+uploaded_files = st.file_uploader(label="combine pdfs", type=['pdf'], accept_multiple_files=True, key=None, disabled=False, label_visibility="collapsed")
