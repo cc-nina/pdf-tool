a92bedf75c6b5312269e0fa798aa8cb42d4b5b09
cc-nina <134860155+cc-nina@users.noreply.github.com>
2024-12-21 21:55:07 +0000

    add widget

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
  
 st.write("""
-# My first app
-Hello *world!*
-""")
+# PDF Combiner""")
+st.write("""
+# PDF Combiner""")
+
+st.file_uploader(label="pdf", type=['pdf'], accept_multiple_files=True, key=None, disabled=False, label_visibility="visible")
