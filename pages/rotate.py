a52041e61b44143cdf69bbb2c825ad0175743918
cc-nina <134860155+cc-nina@users.noreply.github.com>
2024-12-22 23:12:26 +0000

    sequence out of range for rotate

--- pages/rotate.py
+++ pages/rotate.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from pypdf import PdfReader, PdfWriter
+import PyPDF2
 
 st.set_page_config(
     page_title="Rotate",
@@ -18,40 +19,77 @@ name = st.text_input(label='name of pdf',
                      disabled=False, 
                      label_visibility="collapsed")
 
-uploaded_files = st.file_uploader(label="combine pdfs", 
+uploaded_file = st.file_uploader(label="combine pdfs", 
                                   type=['pdf'], 
-                                  accept_multiple_files=True, 
-                                  key=None, disabled=False, 
+                                  accept_multiple_files=False, 
+                                  key=None, 
+                                  disabled=False, 
                                   label_visibility="collapsed")
 
-st.write("""
-Pages to rotate:""")
-first = st.number_input(label='First page', 
-                      min_value=None, 
-                      max_value=None, 
-                      value="min", 
-                      step=None, 
-                      format=None, 
-                      key=None, 
-                      help=None, 
-                      on_change=None, 
-                      args=None, 
-                      kwargs=None, 
-                      placeholder=None, 
-                      disabled=False, 
-                      label_visibility="visible")
+if uploaded_file:
+    name = name+".pdf"
+    file_name = name
+    # not working
+    # page_count = len(uploaded_file.pages)
 
-last = st.number_input(label='Last page', 
-                      min_value=None, 
-                      max_value=None, 
-                      value="min", 
-                      step=None, 
-                      format=None, 
-                      key=None, 
-                      help=None, 
-                      on_change=None, 
-                      args=None, 
-                      kwargs=None, 
-                      placeholder=None, 
-                      disabled=False, 
-                      label_visibility="visible")
+    # after downloading pypdf2
+    pdfReader = PyPDF2.PdfReader(uploaded_file)
+    total = len(pdfReader.pages)
+
+    st.write("""
+    Pages to rotate:""")
+    first = st.number_input(label='This page', 
+                        min_value=1, 
+                        max_value=total, 
+                        value=1, 
+                        step=1, 
+                        placeholder=None, 
+                        disabled=False, 
+                        label_visibility="visible")
+
+    last = st.number_input(label='To this page', 
+                        min_value=first, 
+                        max_value=total, 
+                        value=first, 
+                        step=1, 
+                        placeholder=None, 
+                        disabled=False, 
+                        label_visibility="visible")
+    
+    amnt = st.number_input(label='How many times clockwise', 
+                        min_value=0, 
+                        value=first, 
+                        step=1, 
+                        placeholder=None, 
+                        disabled=False, 
+                        label_visibility="visible")
+    amnt = amnt % 4
+    amnt = amnt * 90
+    
+    reader = PdfReader(uploaded_file)
+    writer = PdfWriter()
+    
+    for i in range((first - 1), last):
+        writer.add_page(reader.pages[i])
+        writer.pages[i].rotate(amnt)
+        
+    for i in range(last - 1, total):
+        writer.add_page(reader.pages[i])
+    
+    with open(name, "wb") as fp:
+        writer.write(fp)
+    
+    with open(name, "rb") as pdf_file:
+        rotatedfile = pdf_file.read()
+
+    st.download_button(label='Download rotated pdf', 
+                   data=rotatedfile, 
+                   file_name=file_name,
+                   type="secondary", 
+                   icon=":material/download:", 
+                   disabled=False, 
+                   use_container_width=False)
+    writer.close()
+else:
+    st.write("Upload your files")
+
